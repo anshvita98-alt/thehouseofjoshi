e49f25761ad9c27346aaee4d3d5617fd45e14122
Queen Joshi <anshujoshi20@gmail.com>
2026-05-28 22:36:23 +0530

    fix: use valid Vercel runtime value

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,7 @@
-import { createServer } from 'http';
 import { startInstance } from '../src/start';
 
 export const config = {
-  runtime: 'nodejs18.x',
-  regions: ['all'],
+  runtime: 'nodejs',
 };
 
 export default startInstance.fetch;
